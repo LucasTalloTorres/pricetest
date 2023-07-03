d02a8160dbccccb49197ed4da36f38a72346f849
Lucas <lucas.tallo.torres@gmail.com>
2023-07-03 17:34:48 +0200

    Create test to Price domain object

---
+++ src/test/java/org/example/domain/model/PriceTest.java
@@ -0,0 +1,33 @@
+package org.example.domain.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+public class PriceTest {
+
+    @Test
+    public void testPriceObjectCreation() {
+        int brandId = 1;
+        LocalDateTime startDate = LocalDateTime.of(2022, 1, 1, 0, 0);
+        LocalDateTime endDate = LocalDateTime.of(2022, 1, 31, 23, 59);
+        int priceList = 1;
+        int productId = 12345;
+        int priority = 1;
+        double price = 19.99;
+        String currency = "EUR";
+
+        Price priceObject = new Price(brandId, startDate, endDate, priceList, productId, priority, price, currency);
+
+        // Assert
+        Assertions.assertEquals(brandId, priceObject.getBrandId());
+        Assertions.assertEquals(startDate, priceObject.getStartDate());
+        Assertions.assertEquals(endDate, priceObject.getEndDate());
+        Assertions.assertEquals(priceList, priceObject.getPriceList());
+        Assertions.assertEquals(productId, priceObject.getProductId());
+        Assertions.assertEquals(priority, priceObject.getPriority());
+        Assertions.assertEquals(price, priceObject.getPrice());
+        Assertions.assertEquals(currency, priceObject.getCurrency());
+    }
+}
