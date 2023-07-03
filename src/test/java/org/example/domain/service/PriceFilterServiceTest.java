7e4898134efdfc5dc83cdaca036782b8aa3ff3d4
LucasTalloTorres <43947359+LucasTalloTorres@users.noreply.github.com>
2023-07-03 18:54:30 +0200

    Merge pull request #2
    * Create test to Price Filter Service
    
    * Modify assertion test
    
    * Modify assertion test
    
    * Create Service and exception

---
+++ src/test/java/org/example/domain/service/PriceFilterServiceTest.java
@@ -0,0 +1,58 @@
+package org.example.domain.service;
+
+import org.example.domain.model.Price;
+import org.example.domain.model.PriceTest;
+import org.example.domain.service.exception.PriceNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PriceFilterServiceTest {
+
+    private PriceFilterService priceFilterService;
+
+    @BeforeEach
+    public void setup() {
+        priceFilterService = new PriceFilterService();
+    }
+
+    @Test
+    public void testPriceNotFoundException_WhenNoPricesFound() {
+        int productId = 12345;
+        int brandId = 1;
+        LocalDateTime dateTime = LocalDateTime.of(2022, 1, 1, 12, 0);
+        List<Price> prices = new ArrayList<>();
+
+        Assertions.assertThrows(PriceNotFoundException.class, () -> priceFilterService.getActivePrice(prices, productId, brandId, dateTime));
+    }
+
+    @Test
+    public void testReturnActivePrice_WhenPriceIsFound() {
+        int productId = 12345;
+        int brandId = 1;
+        LocalDateTime dateTime = LocalDateTime.of(2022, 1, 1, 12, 0);
+
+        List<Price> prices = new ArrayList<>();
+
+        prices.add(Price.builder()
+                .startDate(LocalDateTime.of(2021, 12, 1, 0, 0))
+                .endDate(LocalDateTime.of(2022, 1, 31, 23, 59))
+                .brandId(1).priceList(1).productId(12345).priority(0).price(29.99).currency("EUR").build());
+        prices.add(Price.builder()
+                .startDate(LocalDateTime.of(2022, 1, 1, 0, 0))
+                .endDate(LocalDateTime.of(2022, 1, 31, 23, 59))
+                .brandId(1).priceList(2).productId(12345).priority(1).price(25.99).currency("EUR").build());
+        prices.add(Price.builder()
+                .startDate(LocalDateTime.of(2022, 2, 1, 0, 0))
+                .endDate(LocalDateTime.of(2022, 2, 28, 23, 59))
+                .brandId(2).priceList(3).productId(12345).priority(1).price(22.99).currency("EUR").build());
+
+        Price result = priceFilterService.getActivePrice(prices, productId, brandId, dateTime);
+        Assertions.assertEquals(25.99, result.getPrice());
+        Assertions.assertEquals(2, result.getPriceList());
+    }
+}
