d6ecb3ad8f835dc7b740bd0c9b70d4d2b197c59d
Lucas <lucas.tallo.torres@gmail.com>
2023-07-03 20:23:47 +0200

    Create use case

--- src/main/java/org/example/application/Application.java
+++ src/main/java/org/example/application/Application.java
@@ -3,8 +3,12 @@ package org.example.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@ComponentScan(basePackages = "org.example")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
